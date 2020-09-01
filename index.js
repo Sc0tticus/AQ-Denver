e145fbcf501a4e69547c1660f14b2d24b6a2dba1
Sc0tticus <ssinger303@gmail.com>
2020-09-01 16:18:40 -0600

    Added index.js with Fetch

---
+++ index.js
@@ -0,0 +1,15 @@
+const baseURL = 'http://localhost:4000/air_qualities';
+
+const PM2_5_Data = fetch(baseURL)
+  .then(response => response.json())
+  .then(console.log);
+
+  function heatMapData(geojson){
+    console.log(geojson)
+    return { lat: o.geometry.coordinates[1], lng: o.geometry.coordinates[0], value: o.properties.snr_read }
+  }
+  
+  
+  // = geojson.features.map((o) => {
+  //   return { lat: o.geometry.coordinates[1], lng: o.geometry.coordinates[0], value: o.properties.snr_read }
+  // })
